642610114ee815dc644daeeb4cb5b047eac7c2e0
ckouta <ckouta25@gmail.com>
2019-09-03 11:18:09 -0400

    Initial commit

---
+++ src/main/java/com/estetiCloud/EsteticCloudApplication.java
@@ -0,0 +1,13 @@
+package com.estetiCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsteticCloudApplication.class, args);
+	}
+
+}
