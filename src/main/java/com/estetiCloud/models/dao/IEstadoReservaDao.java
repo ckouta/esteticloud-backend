a146a87701dd4160136fe22bb5a1841d7c543fbc
MiniChii <f.p.chii@gmail.coom>
2019-10-17 11:07:51 -0300

    Daos: EstadoReserva, Role, Usuario

---
+++ src/main/java/com/estetiCloud/models/dao/IEstadoReservaDao.java
@@ -0,0 +1,11 @@
+package com.estetiCloud.models.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.estetiCloud.models.entity.estado_reserva;
+
+public interface IEstadoReservaDao extends JpaRepository<estado_reserva, Long>{
+
+	
+
+}
